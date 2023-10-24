1888f88390bb71ec0871b50a5bc58b851112a9c4
Huy Lam <huy.lam0199@gmail.com>
2023-10-25 03:53:07 +0700

    add template id

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -35,7 +35,7 @@ const Contact = () => {
     emailjs
       .send(
         import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "template_q4f00c1",
         {
           from_name: form.name,
           to_name: "Huy Lam - Portfolio",
